9653b34eaa060bd2c9a5ef800d678f66a5c4117b
subhi78 <kuwatsubhi78@gmail.com>
2024-12-11 12:50:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(
 );
 
 // Swagger
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Routes
 app.use("/", authRoutes);
 app.use("/", aksiRoutes);
