aec3159f808fde0bc37402fb4e79aeba53ebe882
subhi78 <kuwatsubhi78@gmail.com>
2024-12-11 14:32:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -23,14 +23,10 @@ app.use(
     credentials: true,
   })
 );
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self' https://vercel.live"
-  );
-  next();
-});
+res.setHeader(
+  "Content-Security-Policy",
+  "script-src-elem 'self' https://vercel.live"
+);
 
 // Swagger
 app.use(
