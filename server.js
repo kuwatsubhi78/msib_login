71042e9749d144def99565e094aab1f826e3c6e9
subhi78 <kuwatsubhi78@gmail.com>
2024-12-11 12:24:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
