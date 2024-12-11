7294773b4a567e04bd58bb1c5a6f0c525840d279
subhi78 <kuwatsubhi78@gmail.com>
2024-12-11 12:10:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(
 );
 
 // Routes
-app.user("/", (req, res) => res.send("Hello World!"));
+app.use("/", (req, res) => res.send("Hello World!"));
 app.use("/", authRoutes);
 app.use("/", aksiRoutes);
 app.use("/", articleRoutes);
